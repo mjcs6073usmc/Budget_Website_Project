dc703c284e5df296aa94ec30a3849a9a9ea813bc
MyRealName <MyRealName@gmail.com>
2025-01-30 14:39:42 -0500

    made indexjs

---
+++ JavaScript/Index.js
@@ -0,0 +1,31 @@
+//CHATGPT HELPED IN THE DEVELOPMENT OF THIS CODE
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    //gets the ID ttoken and sends it to the backend
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+function sendIdTokenToBackend(id_token) {
+    fetch('https://e99a-50-222-186-92.ngrok-free.app/verify-id-token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: id_token }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Backend response:', data);
+        })
+        .catch(error => {
+            console.error('Error sending ID token to backend:', error);
+        });
+}
