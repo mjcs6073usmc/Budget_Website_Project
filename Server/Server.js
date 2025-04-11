b1c21e72d0a36afb3735843075335ebe7ea062c1
christopherjparrett <146243093+christopherjparrett@users.noreply.github.com>
2025-04-10 20:25:08 -0400

    rendser

--- Server/Server.js
+++ Server/Server.js
@@ -24,8 +24,8 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 const PORT = process.env.PORT || 3001;
-// Serve static files (HTML, CSS, JS) from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '..')));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -58,7 +58,7 @@ app.get('/health', (req, res) => {
 
 // Serve the index.html file when the root URL is accessed
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Start the server
